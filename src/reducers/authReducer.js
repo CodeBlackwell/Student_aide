da68b3f8f630e0d28afb87b552373d225ab4824a
LeChristopher Blackwell <codeblackwell@gmail.com>
2018-04-17 18:41:18 -0600

    clear errors on auth/logout

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -3,9 +3,9 @@ import {AUTH_USER, UNAUTH_USER, AUTH_ERROR, SIGNUP_ERROR} from '../actions/const
 export default function (state = { authenticated: false, error: null }, action) {
     switch (action.type) {
         case AUTH_USER:
-            return {...state, authenticated: true};
+            return {...state, authenticated: true, error: ''};
         case UNAUTH_USER:
-            return {...state, authenticated: false};
+            return {...state, authenticated: false, error: ''};
         case AUTH_ERROR:
             return {...state, error: action.payload}
     }
